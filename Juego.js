081f2bc04d3f1df31a55c1855a657fae14507843
Nucion <nucion@users.noreply.github.com>
2020-06-15 20:50:50 -0300

    Se agregaron las funciones de movimiento

--- Juego.js
+++ Juego.js
@@ -32,6 +32,7 @@ function DibujarCuadriculado() {
         i = i + 400/10;
     }
     ctx.closePath();
+    DibujarJugador ();
 }
 /**
  * Limpia el canvas para poder crear otro cuadriculado
@@ -53,6 +54,7 @@ function LimpiarCanvas() {
  * @param {number} value-el numero de celdas
  * @return tablero
  */
+/**
 function crearTablero(value){
     if(value==5){
         var tablero=
@@ -95,49 +97,78 @@ function crearTablero(value){
                 [0,0,0,0,1,0,0,0,0,0,1,0,0,0,1]];
     }
     return tablero;
+}*/
+var tablero=
+        [[0,0,0,1,0,0,0,1,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,1,0,0,0,1,0,0,1,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,1,0,0,0,0,0,0,1],
+        [0,0,0,0,0,0,1,0,0,0],
+        [1,0,0,0,0,1,0,0,0,0],
+        [0,0,3,1,0,0,0,1,0,0],
+        [0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,1]
+    ];
+var posicionX=20;
+var posicionY=20;
+var vidas=6;
+
+    function DibujarJugador () {
+        var canvas= document.getElementById(elementid="myCanvas");
+        var ctx= canvas.getContext("2d");
+
+
+        ctx.arc(x=posicionX, y=posicionY, radius=14, starAngle=0, endAngle=2*Math.PI);
+        ctx.fillStyle = "#2233aa";
+        ctx.fill();
+
+
 }
-var t=creartablero(value);
-var pX=0;
-var pY=0;
-var vidas=(document.getElementById(elementid="posicion").value)/2+1;
 
-function MoverIzquierda(){
-    if(pX>0)pX=pX-1;
-    if (t[pX][pY]==1) {
-        vidas = vidas - 1;
-        alert("BOMBA!");
-    }
+function MoverAbajo(){
+    var canvas= document.getElementById(elementid="myCanvas");
+    var ctx= canvas.getContext("2d");
 
+    if(posicionY<380)posicionY+=40;
+LimpiarCanvas();
+DibujarCuadriculado();
 }
 function MoverDerecha(){
-    if(pX<(document.getElementById(elementid="posicion").value)-1)pX=pX+1;
-    if (t[pX][pY]==1){
-        vidas=vidas-1;
-        alert("BOMBA!");
-    }
+    var canvas= document.getElementById(elementid="myCanvas");
+    var ctx= canvas.getContext("2d");
 
+    if(posicionX<380)posicionX+=40;
+    LimpiarCanvas();
+    DibujarCuadriculado();
 }
+
+function MoverIzquierda(){
+    var canvas= document.getElementById(elementid="myCanvas");
+    var ctx= canvas.getContext("2d");
+
+   if(posicionX>20) posicionX-=40;
+    LimpiarCanvas();
+    DibujarCuadriculado();
+}
+
 function MoverArriba(){
-    if(pY>0)pY=pY-1;
-    if (t[pX][pY]==1) {
-        vidas = vidas - 1;
-        alert("BOMBA!");
-    }
+    var canvas= document.getElementById(elementid="myCanvas");
+    var ctx= canvas.getContext("2d");
+if(posicionY>20)  posicionY-=40;
 
+    LimpiarCanvas();
+    DibujarCuadriculado();
 }
-function MoverAbajo(){
-    if(pY<(document.getElementById(elementid="posicion").value)-1)pY=pY+1;
-    if (t[pX][pY]==1) {
-        vidas = vidas - 1;
-        alert("BOMBA!");
 
-    }
-}
-function Explorar(){
-    var contador=0;
-    if(pX>0&&t[pX-1][pY]==1)contador++;
-    if(pY>0&&t[pX][pY-1]==1)contador++;
-    if(pY<(document.getElementById(elementid="posicion").value)&&t[pX][pY+1])contador++;
-    if(pX<(document.getElementById(elementid="posicion").value)&&t[pX+1][pY])contador++;
-    return contador;
+
+
+
+
+
+function LimpiarCanvas1(){
+        LimpiarCanvas();
+    posicionX=20;
+    posicionY=20;
+
 }
